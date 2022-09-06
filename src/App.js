1af516dd06e5198c42bf73728cf4adc25ba6ba86
Hamza405 <hamzash863@gmail.com>
2022-09-06 17:44:08 -0400

    Implement Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TopBar from "./components/TopBar/TopBar";
-// import HomePage from "./pages/HomePage/HomePage";
-// import SinglePostPage from "./pages/SinglePostPage/SinglePostPage";
-// import WritePage from "./pages/WritePage/WritePage";
-// import SettingsPage from "./pages/SettingsPage/SettingsPage";
-// import LoginPage from "./pages/Auth/LoginPage";
+import HomePage from "./pages/HomePage/HomePage";
+import SinglePostPage from "./pages/SinglePostPage/SinglePostPage";
+import WritePage from "./pages/WritePage/WritePage";
+import SettingsPage from "./pages/SettingsPage/SettingsPage";
+import LoginPage from "./pages/Auth/LoginPage";
 import SignupPage from "./pages/Auth/SignupPage";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <TopBar />
-      <SignupPage />
-    </>
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="/post/:postId" element={<SinglePostPage />} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/pages/WritePage/WritePage.jsx
+++ src/pages/WritePage/WritePage.jsx
@@ -1,5 +1,4 @@
 import Write from "../../components/Write/Write";
-import style from "./WritePage.module.css";
 
 const WritePage = () => {
   return <Write />;
